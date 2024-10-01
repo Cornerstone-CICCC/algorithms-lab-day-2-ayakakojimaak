4fa92d5b3d6537622bc96e2494b44b4f1baef136
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-01 12:15:05 -0700

    Update ex10.js

--- ex10.js
+++ ex10.js
@@ -2,4 +2,14 @@
 // with words separated by spaces or underscores, and returns the string in camelCase format.
 // Example: convertToCamelCase("hello_world") should return "helloWorld".
 
+const convertToCamelCase = (string) => {
+  const regexSpaces = /\s([a-z])/g;
+  const regexUnderscores = /_([a-z])/g;
+  const replaceString = (regex, str) => {
+    return str.replace(regex, function (match) {
+      return match.slice(1).toUpperCase();
+    });
+  };
+  return replaceString(regexSpaces, replaceString(regexUnderscores, string));
+};
 console.log(convertToCamelCase("hello_world")); // Expected output: "helloWorld"
