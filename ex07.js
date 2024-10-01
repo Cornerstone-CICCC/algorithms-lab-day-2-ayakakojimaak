5bd0d29a64a0acbfe70dd3941f7147cd89900aaa
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-01 11:24:54 -0700

    Update ex07.js

--- ex07.js
+++ ex07.js
@@ -2,6 +2,12 @@
 // and returns true if the email is valid, and false otherwise. Use basic validation
 // (e.g., the presence of "@" and a ".").
 // Example: validateEmail("test@example.com") should return true.
-
+const validateEmail = (string) => {
+  if (string.includes("@") && string.includes(".")) {
+    return true;
+  } else {
+    return false;
+  }
+};
 console.log(validateEmail("test@example.com")); // Expected output: true
 console.log(validateEmail("invalid-email.com")); // Expected output: false
