4e792cfb1ab549e35404e605bd9776f4afb3caf8
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-01 10:57:28 -0700

    Update ex05.js

--- ex05.js
+++ ex05.js
@@ -2,4 +2,11 @@
 // and returns the number of consonants in the string.
 // Example: countConsonants("hello world") should return 7.
 
+const countConsonants = (string) => {
+  const vowel = ["a", "i", "u", "e", "o"];
+  const targetArray = Array.from(string.replace(" ", ""));
+  const result = targetArray.filter((item) => !vowel.includes(item));
+  return result.length;
+};
+
 console.log(countConsonants("hello world")); // Expected output: 7
