aceec02637436e0462f293ecfea372bd47c48a94
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-01 09:14:01 -0700

    Update ex03.js

--- ex03.js
+++ ex03.js
@@ -2,4 +2,9 @@
 // with one number missing and returns the missing number.
 // Example: findMissingNumber([1, 2, 4, 5]) should return 3.
 
+const findMissingNumber = (array) => {
+  const max = array[array.length - 1];
+  const referenceNumber = [...Array(max).keys()].map((i) => i + 1);
+  return referenceNumber.filter((item) => !array.includes(item));
+};
 console.log(findMissingNumber([1, 2, 4, 5])); // Expected output: 3
