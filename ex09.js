e120b6717074e0a7e84856cdc20696b33f264bdc
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-01 11:32:40 -0700

    Update ex09.js

--- ex09.js
+++ ex09.js
@@ -2,5 +2,15 @@
 // and a property name, and returns the array sorted by that property in ascending order.
 // Example: sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")
 // should return [{name: "Bob", age: 25}, {name: "Alice", age: 30}].
-
-console.log(sortByProperty([{name: "Alice", age: 30}, {name: "Bob", age: 25}], "age")); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
+const sortByProperty = (array, property) => {
+  return array.sort((a, b) => a[property] - b[property]);
+};
+console.log(
+  sortByProperty(
+    [
+      { name: "Alice", age: 30 },
+      { name: "Bob", age: 25 },
+    ],
+    "age"
+  )
+); // Expected output: [{name: "Bob", age: 25}, {name: "Alice", age: 30}]
