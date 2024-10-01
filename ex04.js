96282766810c4d01d77393034c0b8bd0f2f846c8
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-01 09:41:37 -0700

    Update ex04.js

--- ex04.js
+++ ex04.js
@@ -2,4 +2,19 @@
 // that appears the most times. If there is a tie, return the first character that reaches the maximum frequency.
 // Example: mostFrequentChar("javascript") should return "a".
 
+const mostFrequentChar = (string) => {
+  const result = { maximum: 0, character: "" };
+  const array = Array.from(string).filter((element, index) => {
+    return Array.from(string).indexOf(element) === index;
+  });
+  for (let index = 0; index < array.length; index++) {
+    const targetCharacter = array[index];
+    const count = string.match(new RegExp(targetCharacter, "g"));
+    if (count.length > result.maximum) {
+      result.maximum = count.length;
+      result.character = targetCharacter;
+    }
+  }
+  return result.character;
+};
 console.log(mostFrequentChar("javascript")); // Expected output: "a"
