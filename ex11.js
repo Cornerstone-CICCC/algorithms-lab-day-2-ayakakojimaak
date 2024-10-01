74819cb8bdf5db1bf2f0eef1c4291e62cea0d758
ayakakojimaak <125942459+ayakakojimaak@users.noreply.github.com>
2024-10-01 12:22:21 -0700

    Update ex11.js

--- ex11.js
+++ ex11.js
@@ -2,4 +2,8 @@
 // and returns the number of times the word appears in the string.
 // Example: countWordOccurrences("hello world hello", "hello") should return 2.
 
+const countWordOccurrences = (string, word) => {
+  const regex = new RegExp(word, "g");
+  return string.match(regex).length;
+};
 console.log(countWordOccurrences("hello world hello", "hello")); // Expected output: 2
